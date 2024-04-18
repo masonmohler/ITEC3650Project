f040d479bc60c10778bc902a911a83591f3414b5
masonmohler <112035573+masonmohler@users.noreply.github.com>
2024-04-18 22:14:18 +0000

    Worked on API integration

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,93 @@ $(".fade").slick({
   cssEase: "linear",
   centerPadding: "1rem",
 });
+
+async function getData() {
+  "use strict";
+
+  // Get a reference to the form - Use the ID of the form
+  let form = $("#myform");
+
+  // Validate all of the form elements
+  form.validate();
+
+  // If all of the form elements are valid, the get the form values
+  if (form.valid()) {
+    let siteChoice = document.getElementById("siteChoice").value;
+
+    /* URL for AJAX Call */
+    let myURL =
+      "https://waterservices.usgs.gov/nwis/iv/?format=json&sites=" +
+      siteChoice +
+      "&indent=on&period=P7D&siteStatus=all&parameterCd=00065";
+    let msgObject = await fetch(myURL);
+    if (msgObject.status >= 200 && msgObject.status <= 299) {
+      let msgJSONText = await msgObject.text();
+      // Parse the JSON string into an object
+      let msg = JSON.parse(msgJSONText);
+
+      let dates = [];
+      let values = [];
+      /* fLen contains the length of the array (number of values) */
+      let fLen = msg.value.timeSeries[0].values[0].value.length;
+      for (let i = 0; i < fLen; i++) {
+        values[i] = msg.value.timeSeries[0].values[0].value[i].value;
+        dates[i] = new Date(
+          msg.value.timeSeries[0].values[0].value[i].dateTime
+        ).toLocaleString();
+      }
+      let sitename = msg.value.timeSeries[0].sourceInfo.siteName;
+      let sitecode = msg.value.timeSeries[0].sourceInfo.siteCode[0].value;
+      let siteDescription =
+        msg.value.timeSeries[0].variable.variableDescription;
+
+      let currentLevel = values[fLen - 1];
+
+      document.getElementById("currentWaterLevel").innerHTML =
+        "The current water level of the " +
+        sitename +
+        " is " +
+        currentLevel +
+        " feet deep.";
+
+      createChart(values, dates, sitename);
+    } else {
+      /* AJAX completed with error - probably invalid stock ticker symbol */
+      alert("Site Data Not Found - Status: " + msgObject.status);
+      return;
+    }
+  }
+}
+
+function createChart(values, dates, sitename) {
+  let ctx0 = document.getElementById("chartjs-0");
+  var myChart = new Chart(ctx0, {
+    type: "line",
+    data: {
+      labels: dates,
+      datasets: [
+        {
+          label: sitename,
+          data: values,
+          fill: true,
+          backgroundColor: "rgb(242, 113, 39, 0.5)",
+          borderColor: "rgb(242, 113, 39)",
+          lineTension: 0.1,
+        },
+      ],
+    },
+    options: {
+      intersect: true,
+      responsive: false,
+      maintainAspectRatio: true,
+      interaction: {
+        // Overrides the global setting
+        mode: "index",
+      },
+      title: {
+        display: true,
+        text: "River Level 7 Day History",
+      },
+    },
+  });
+}
